9196af7b2cda7a60ce4aadd7bd86648fa833fcda
Lena Teresa Will <my.services1@icloud.com>
2024-08-18 12:57:04 +0100

    add elastic net model week 5

--- weekly_models_elastic_net.R
+++ weekly_models_elastic_net.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(glmnet)
 
 # load text-based indicators ---------------------------------------------------
+
 attention <- read.csv("/Users/lena/Documents/R/master_thesis/attention_issue_mapping.csv") %>%
   select(-X) %>% 
   mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
@@ -15,7 +16,8 @@ attention$month <- as.Date(sub("\\d{2}$", "1", attention$Date))
 
 attention <- attention %>% 
   relocate(month, .before = quarter_avail) %>% 
-  slice(-1)
+  slice(-1) %>% 
+  filter(Date < "2024-04-01")
 
 # attention <- attention %>%
 #   relocate(quarter_avail, .after = Date) %>%
@@ -33,7 +35,7 @@ attention_bridge <- attention_bridge %>%
 
 # esi
 
-esi <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/esi.csv") %>%
+esi <- read.csv("/Users/lena/Documents/R/master_thesis/data/esi.csv") %>%
   select(-X) %>%
   mutate(week = case_when(
     month(Date) == 1 |
@@ -45,11 +47,29 @@ esi <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/esi.csv"
       month(Date) == 8 |
       month(Date) == 11 ~ 9,
     month(Date) == 3 |
-      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 13))
+      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 13)) %>% 
+  filter(Date >= "2001-10-01" & Date < "2024-04-01") %>% 
+  relocate(week, .after = Date)
+
+esi_bridge <- esi %>%
+  mutate(across(all_of(c(
+    3:ncol(esi)
+  )), ~ ., .names = "{col}_b"))
+
+skip_esi <- ncol(esi) - 2
+for (ii in 1:nrow(esi_bridge)) {
+  if (esi_bridge$week[ii] == 4) {
+    esi_bridge[ii, 3 + skip_esi] = esi_bridge[ii, 3]
+  } else if (esi_bridge$week[ii] == 9) {
+    esi_bridge[ii, 3 + skip_esi] = (esi_bridge[ii, 3] + esi_bridge[ii - 1, 3]) / 2
+  } else {
+    esi_bridge[ii, 3 + skip_esi] = (esi_bridge[ii, 3] + esi_bridge[ii - 1, 3] + esi_bridge[ii - 2, 3]) / 3
+  }
+}
 
 # cpi
 
-cpi <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/cpi.csv") %>%
+cpi <- read.csv("/Users/lena/Documents/R/master_thesis/data/cpi.csv") %>%
   select(-X) %>%
   mutate(week = case_when(
     month(Date) == 1 |
@@ -61,11 +81,30 @@ cpi <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/cpi.csv"
       month(Date) == 8 |
       month(Date) == 11 ~ 9,
     month(Date) == 3 |
-      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 13))
+      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 13)) %>% 
+  filter(Date >= "2001-10-01" & Date < "2024-04-01") %>% 
+  relocate(week, .after = Date)
+
+cpi_bridge <- cpi %>%
+  mutate(across(all_of(c(
+    3:ncol(cpi)
+  )), ~ ., .names = "{col}_b"))
+
+skip_cpi <- ncol(cpi) - 2
+for (ii in 1:nrow(cpi_bridge)) {
+  if (cpi_bridge$week[ii] == 4) {
+    cpi_bridge[ii, 3 + skip_cpi] = cpi_bridge[ii, 3]
+  } else if (cpi_bridge$week[ii] == 9) {
+    cpi_bridge[ii, 3 + skip_cpi] = (cpi_bridge[ii, 3] + cpi_bridge[ii - 1, 3]) / 2
+  } else {
+    cpi_bridge[ii, 3 + skip_cpi] = (cpi_bridge[ii, 3] + cpi_bridge[ii - 1, 3] + cpi_bridge[ii - 2, 3]) / 3
+  }
+}
+
 
 # vacancies 
 
-vacancies <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/vacancies.csv") %>%
+vacancies <- read.csv("/Users/lena/Documents/R/master_thesis/data/vacancies.csv") %>%
   select(-X) %>%
   mutate(week = case_when(
     month(Date) == 1 |
@@ -77,11 +116,29 @@ vacancies <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/va
       month(Date) == 8 |
       month(Date) == 11 ~ 9,
     month(Date) == 3 |
-      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 13))
+      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 13)) %>% 
+  filter(Date >= "2001-10-01" & Date < "2024-04-01") %>% 
+  relocate(week, .after = Date)
+
+vacancies_bridge <- vacancies %>%
+  mutate(across(all_of(c(
+    3:ncol(vacancies)
+  )), ~ ., .names = "{col}_b"))
+
+skip_vacancies <- ncol(vacancies) - 2
+for (ii in 1:nrow(vacancies_bridge)) {
+  if (vacancies_bridge$week[ii] == 4) {
+    vacancies_bridge[ii, 3 + skip_vacancies] = vacancies_bridge[ii, 3]
+  } else if (vacancies_bridge$week[ii] == 9) {
+    vacancies_bridge[ii, 3 + skip_vacancies] = (vacancies_bridge[ii, 3] + vacancies_bridge[ii - 1, 3]) / 2
+  } else {
+    vacancies_bridge[ii, 3 + skip_vacancies] = (vacancies_bridge[ii, 3] + vacancies_bridge[ii - 1, 3] + vacancies_bridge[ii - 2, 3]) / 3
+  }
+}
 
 # term spread
 
-term_spread <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/term_spread.csv") %>%
+term_spread <- read.csv("/Users/lena/Documents/R/master_thesis/data/term_spread.csv") %>%
   select(-X) %>%
   mutate(week = case_when(
     month(Date) == 1 |
@@ -93,11 +150,29 @@ term_spread <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/
       month(Date) == 8 |
       month(Date) == 11 ~ 11,
     month(Date) == 3 |
-      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 0))
+      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 0)) %>% 
+  filter(Date >= "2001-10-01" & Date < "2024-04-01") %>% 
+  relocate(spread, .after = last_col())
+
+term_spread_bridge <- term_spread %>%
+  mutate(across(all_of(c(
+    4:ncol(term_spread)
+  )), ~ ., .names = "{col}_b"))
+
+skip_term_spread <- ncol(term_spread) - 3
+for (ii in 1:nrow(term_spread_bridge)) {
+  if (term_spread_bridge$week[ii] == 7) {
+    term_spread_bridge[ii, 4 + skip_term_spread] = term_spread_bridge[ii, 4]
+  } else if (term_spread_bridge$week[ii] == 11) {
+    term_spread_bridge[ii, 4 + skip_term_spread] = (term_spread_bridge[ii, 4] + term_spread_bridge[ii - 1, 4]) / 2
+  } else {
+    term_spread_bridge[ii, 4 + skip_term_spread] = term_spread_bridge[ii, 4]
+  }
+}
 
 # ip index 
 
-ip_index <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/ip_index.csv") %>%
+ip_index <- read.csv("/Users/lena/Documents/R/master_thesis/data/ip_index.csv") %>%
   select(-X) %>%
   mutate(week = case_when(
     month(Date) == 1 |
@@ -109,14 +184,18 @@ ip_index <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/ip_
       month(Date) == 8 |
       month(Date) == 11 ~ 0,
     month(Date) == 3 |
-      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 0))
+      month(Date) == 6 | month(Date) == 9 | month(Date) == 12 ~ 0)) %>% 
+  filter(Date >= "2001-10-01" & Date < "2024-04-01") %>% 
+  relocate(IP, .after = last_col())
+
 
 # gdp 
 
-gdp <- read.csv("/Users/lena/Documents/R/master_thesis/attention_bridge/gdp_weekly.csv") %>% 
-  rename(week = quarter_week)
+gdp <- read.csv("/Users/lena/Documents/R/master_thesis/data/gdp_weekly.csv") %>% 
+  rename(week = quarter_week) %>% 
+  filter(Date >= "2001-10-01" & Date < "2024-04-01")
 
-#Period 1: Recession - trainings sample: 2005Q1-2007Q2
+#Period 1: Recession - training sample: 2005Q1-2007Q2
 
 min_train <- "2001-10-01"
 min_test <- "2007-07-01"
@@ -126,89 +205,31 @@ max_test <- "2009-06-01"
 
 source("/Users/lena/Git/master-thesis/functions/elasticNet_w1.R")
 
-week1 <- elasticNet_w1(gdp, attention, min_train, min_test, max_test)
+week1 <- elasticNet_w1(gdp, attention_bridge, min_train, min_test, max_test)
 
 # Model 2
 
-y_m1 <- gdp %>%
-  filter(week == 1)
-X_m1 <- attention %>%
-  filter(week_avail == 1)
+source("/Users/lena/Git/master-thesis/functions/elasticNet_w2.R")
 
-# define nowcasting window
-window <- y_m1 %>%
-  select(Date) %>%
-  filter(Date >= min_test & Date <= max_test)
-window <- as.matrix(window)
+week2 <- elasticNet_w2(gdp, attention_bridge, min_train, min_test, max_test)
 
-# initiate result vectors
+# Model 3
 
-predictions <- c()
-oos_error <- c()
-oos_error_all <- NULL
-rmsfe <- NULL
+source("/Users/lena/Git/master-thesis/functions/elasticNet_w3.R")
 
-alpha_ini <- as.matrix(seq(
-  from = 0.1,
-  to = 0.9,
-  length.out = 9
-))
+week3 <- elasticNet_w3(gdp, attention_bridge, min_train, min_test, max_test)
 
-# nowcasting
+# Model 4
 
-for (ii in 1:nrow(alpha_ini)) {
-  for (month in 1:nrow(window)) {
-    window_test <- window[month]
-    y_m1_train <- y_m1 %>%
-      filter(Date >= min_train & Date < window_test) %>%
-      select(gdp_growth)
-    y_m1_train <- as.matrix(y_m1_train)
-    X_m1_train <- X_m1 %>%
-      filter(month >= min_train & month < window_test) %>%
-      select(-c(month, Date, week_avail, quarter_avail))
-    
-    mean_x <- apply(X_m1_train, 2, mean)
-    sd_x <- apply(X_m1_train, 2, sd)
-    
-    X_m1_train_z <- scale(X_m1_train, center = mean_x, scale = sd_x)
-    
-    fit_en <-
-      cv.glmnet(
-        X_m1_train_z,
-        y_m1_train,
-        alpha = alpha_ini[ii],
-        type.measure = "mse",
-        nfolds = 10,
-        family = "gaussian"
-      )
-    
-    X_m1_test <- X_m1 %>%
-      filter(month == window_test) %>%
-      select(-c(month, Date, week_avail, quarter_avail))
-    X_m1_test_z <- scale(X_m1_test, center = mean_x, scale = sd_x)
-    
-    y_m1_test <- y_m1 %>%
-      filter(Date == window_test) %>%
-      select(gdp_growth)
-    y_m1_test <- as.matrix(y_m1_test)
-    
-    predict_en <-
-      predict(fit_en, s = fit_en$lambda.1se, newx = X_m1_test_z)
-    
-    predictions[month] <- predict_en
-    
-    oos_error[month] <- (predict_en - y_m1_test)
-    
-  }
-  oos_error_prep <- t(oos_error)
-  oos_error_all <- oos_error_all %>%
-    rbind(oos_error_prep)
-  rmsfe[ii] <- sqrt(mean(oos_error ^ 2))
-}
-min_rmsfe <- min(rmsfe)
-min_index <- which(rmsfe == min(rmsfe))
-oos_error_min <- oos_error_all[min_index[1], ]
-results <- list(min_rmsfe, oos_error_all, oos_error_min, min_index)
+source("/Users/lena/Git/master-thesis/functions/elasticNet_w4.R")
+
+week4 <- elasticNet_w4(gdp, attention_bridge, esi, vacancies, cpi, min_train, min_test, max_test)
+
+# Model 5
+
+
+
+
 
 
 
